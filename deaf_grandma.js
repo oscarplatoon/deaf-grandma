06c98243185f4203f008789cb7d073abd9378464
jakewhite5 <jakewhite5@yahoo.com>
2021-05-24 21:20:42 -0700

    This is why python is better

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,23 @@
 function deafGrandma() {
-
-}
-
+  let input = require('prompt-sync')();
+  let count = 0;
+  while (count < 2 ) {
+    const retort = input('HELLO??!   ');
+   if (retort == '') {
+      console.log('WHAT?!');
+  } else if (/[a-z]/.test(retort)) {
+      console.log('SPEAK UP KID!!');
+  } else if (retort === 'GOODBYE!') {
+      if (count == 0) {
+        console.log('LEAVING SO SOON?');
+        count ++;
+      }
+    else if (count == 1) {
+      console.log('LATER SKATER!');
+      count ++;
+    }
+  } else {
+    console.log('NOT SINCE 1946')
+  }
+}}
 deafGrandma();
