b59bd00145837a7446c256fd219231d6baf03831
tomprete <prete316@gmail.com>
2021-05-25 10:37:01 -0500

    instructor solution

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,34 @@
-function deafGrandma() {
+var prompt = require('prompt');
 
+prompt.start();
+
+let goodByeCount = 0;
+
+function deafGrandma() {
+  prompt.get(['input'], function (err, result) {
+    const input = result.input;
+    // If you don't input anything (just hit enter) she responds with WHAT?!
+    if (input === '') {
+      console.log("WHAT?!");
+    }
+    // The second time you say GOODBYE! she says LATER, SKATER! and the program exits.
+    else if (input === "GOODBYE!" && goodByeCount === 1) {
+      return console.log("LATER, SKATER!");
+    }
+    // The first time you say GOODBYE! she says LEAVING SO SOON?
+    else if (input === "GOODBYE!") {
+      console.log("LEAVING SO SOON?");
+      goodByeCount++;
+    }
+    // If you talk to her in all upper-case letters, she responds with NO, NOT SINCE 1946!
+    else if (input === input.toUpperCase()) {
+      console.log('NO, NOT SINCE 1946!');
+    }
+    // If you ask a question with any lower-case letters, she responds with SPEAK UP, KID!
+    else {
+      console.log("SPEAK UP, KID!");
+    }
+  });
 }
 
 deafGrandma();
