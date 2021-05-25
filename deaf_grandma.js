4286f249b1a37e7997a818516ebdcda21a889a72
Jeremiah Mauga <miahmauga@gmail.com>
2021-05-24 20:37:11 -0700

    Jeremiah's attempt at deaf-grandma

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,28 @@
-function deafGrandma() {
+function deafGrandma(string) {
+  
+  // Create a variable to keep track of how many GOODBYE! have been said.
+  let goodByeCounter = 0;
+ 
 
+  // Use if statements to go through the string and return the proper response.
+  if (string == null) {
+      console.log("WHAT?!");
+  } else {
+    if (string == string.toLowerCase()) {
+      console.log("SPEAK UP, KID!"); 
+    } 
+    if (string == "GOODBYE!") {
+      goodByeCounter++;
+      console.log("LEAVING SO SOON?")
+      console.log(goodByeCounter);
+    } if (string == "GOODBYE!" && goodByeCounter > 0) {
+      console.log("LATER, SKATER!");
+    } else {
+    if (string == string.toUpperCase()) {
+      console.log("NO, NOT SINCE 1946!")
+      }
+    } 
+  }
 }
 
-deafGrandma();
+deafGrandma("GOODBYE!");
