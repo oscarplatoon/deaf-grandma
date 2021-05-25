dd1b5482b93559d9bafb990c741a7fbb56f48838
Kyle Gunter <kylewgunter@gmail.com>
2021-05-25 08:11:59 -0500

    attempted deaf-grandma challenge

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,22 @@
-function deafGrandma() {
-
+function deafGrandma(str) {
+  let lowerCaseLet = [/a-z/];
+  let upperCaseLet = [/A-Z/];
+  let talk = prompt("Talk to Grandma!");
+  if(str.length === 0){
+    console.log("WHAT?!");
+  } 
+  else if (str.includes(lowerCaseLet) === true){
+    console.log("SPEAK UP, KID!");
+  }
+  else if (str.includes(upperCaseLet) === true){
+    console.log("NO, NOT SINCE 1946!");
+  }
+  else if (str === "GOODBYE!"){
+    console.log("LEAVING SO SOON?")
+  } 
+  else {
+    console.log("LATER, SKATER!")
+  }
 }
 
-deafGrandma();
+deafGrandma('');
