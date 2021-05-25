b5098a6a08f1821a4b2a313caca0957a6548ddb8
ThomasVirn <thomas.virnston1@gmail.com>
2021-05-25 09:14:35 -0500

    solution

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,24 @@
 function deafGrandma() {
+  secondResponse = ''
+  response = prompt("HEY KID!",  )
+  regexLowerCase = new RegExp(/[a-z]/);
+  regexUpperCase = new RegExp(/[A-Z]/);
+  
+  if (response == "") {
+    console.log('WHAT?')
+  } else if (regexLowerCase.test(response) == true) {
+    console.log('SPEAK UP, KID')
+  } else if (response == "GOODBYE!") {
+   secondResponse = prompt('LEAVING SO SOON?', )
+    } else if (secondResponse == "GOODBYE!") {
+      console.log("LATER SKATER!")
+    }
+  else if (regexUpperCase.test(response) == true) {
+    console.log('NO, NOT SINCE 1946!')
+  } 
+  
+  }
+  
+  deafGrandma()
 
-}
-
-deafGrandma();
+  //Copied from my repl.it, couldnt figure out the issues surrounding prompt in this IDE.
