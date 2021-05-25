7db47052a30b0db24802603fe5e9dc150f493cbe
RenoAverill <reno_averill@yahoo.com>
2021-05-24 21:52:50 -0600

    completed deaf_granma.js

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,32 @@
 function deafGrandma() {
+    //requires $ npm install prompt-sync 
+    const prompt = require('prompt-sync')();
+    
+    let hasLowercaseLetter = str => (/[a-z]/.test(str)),
+        hasAllUppercaseLetters = str => (/[A-Z]/.test(str)),
+        count = 0;
+    
+    while(count < 2) {
+        let output = prompt('HEY KID!')
 
+        if (count > 0 && output === 'GOODBYE!') {
+            console.log('LATER SKATER!')
+            return;
+        }
+        else if (output === 'GOODBYE!') {
+            console.log('LEAVING SO SOON?')
+            count++
+        }
+        else if (output === '') console.log('what?!')
+
+        else if (hasLowercaseLetter(output)) console.log('SPEAK UP, KID!') 
+
+        else if (hasAllUppercaseLetters(output)) console.log('NO, NOT SINCE 1946')
+    }
 }
 
-deafGrandma();
+deafGrandma()
+    
+
+    
+    
