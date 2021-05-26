e8c5a66e93dfa354c38adc0a9b3754f7c8654a55
vzbrunstad <vzbrunstad@msn.com>
2021-05-26 14:50:11 -0500

    Vincent B Deaf Grandma Py Complete js in progress

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,27 @@
 def deaf_grandma():
 
+    exit = 0
+ 
+    while exit <=1:
+        response = input("Talk to grandma ")
+        answer = 0
+        if response == "":
+            print("WHAT?")
+            answer += 1
+        elif response.isupper() == 0 and answer == 0:
+            print("SPEAK UP, KID?")
+            answer = 1
+        elif response.isupper() == 1 and answer == 0 and response != "GOODBYE!":
+            print("NO, NOT SINCE 1946!")
+            answer = 1
+        elif response == "GOODBYE!" and exit == 0 and answer == 0:
+            print("LEAVING SO SOON?")
+            exit +=1
+            answer = 1
 
+        elif response == "GOODBYE!" and exit == 1 and answer == 0:
+            print("LATER, SKATER!")
+            answer = 1
+            exit +=1
 
 deaf_grandma()
