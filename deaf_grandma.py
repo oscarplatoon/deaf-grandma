cb5b819d88824024d37957fc6df708e4d9dfcb3b
Jeremiah Mauga <miahmauga@gmail.com>
2021-05-26 16:13:28 -0700

    Jeremiah's python solution for deaf_grandma attempt#5

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -2,28 +2,28 @@ def deaf_grandma():
     
     print("Say something to your Grandma")
     
-    response = input()
+    
     good_bye_counter = 0
     checker = "GOODBYE!"
     
-
-    while response == "" :
-        print("WHAT?!")
+    while good_bye_counter < 2 :
         response = input()
-    while response.islower() :
-        print("SPEAK UP KID")
-        response = input()
-    while response.isupper() and response != checker:
-        print("NO, NOT SINCE 1946!")
-        response = input()
-    while response == checker and good_bye_counter == 0 :
-        good_bye_counter += 1
-        print("LEAVING SO SOON!?")
-        response = input()
-    while response == checker and good_bye_counter == 1 :
-        print("LATER SKATER!")    
-        break
-
+        if response == "" :
+            print("WHAT?!")
+            deaf_grandma()
+        elif response == checker and good_bye_counter == 1 :
+            print("LATER SKATER!")
+            return()
+        elif response == checker :
+            print("LEAVING SO SOON!?")   
+            good_bye_counter += 1
+        elif response == response.upper() :
+            print("NO, NOT SINCE 1946!")
+            deaf_grandma()
+        else :
+            print("SPEAK UP KID!")
+            deaf_grandma()
     
+     
 
 deaf_grandma()
