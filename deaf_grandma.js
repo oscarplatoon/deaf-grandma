8932b20de651f50b71dc7160953905cdb0fb648b
stesun2 <ss27@illinois.edu>
2021-05-26 17:20:02 -0500

    completed deaf_grandma.py

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,12 @@
+var prompt = require('prompt');
+prompt.start();
+
 function deafGrandma() {
 // THIS IS ANOTHER TEST
+
+
+
 }
+
 
 deafGrandma();
--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,25 @@
 def deaf_grandma():
+    prompt = input('HEY KID!')
+    goodbye_count = 0
 
+    # Only exits once input "GOODBYE!" x 2
+    while (goodbye_count != 2):
+        prompt = input()
+        if (prompt == 'GOODBYE!'):
+            if (goodbye_count == 0):
+                goodbye_count += 1
+                print('LEAVING SO SOON?')
+            else:
+                goodbye_count += 1
+            # print(goodbye_count)     
+        elif (prompt == ''):
+            print('WHAT?!')
+        elif (prompt == prompt.lower()):
+            print('SPEAK UP KID!')
+        elif (prompt == prompt.upper()):
+            print('NO, NOT SINCE 1946!')
+
+    print('LATER, SKATER!')
 
 
 deaf_grandma()
