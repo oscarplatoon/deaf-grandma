cc3f8055334f92a9b86b2a272a240735b7c8a0d3
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-05-26 13:38:47 -0500

    Shawn's python solution

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,4 +1,24 @@
 def deaf_grandma():
+    
+    goodbyes = 0
+    print("HI SON!")
+    
+    while goodbyes < 2:
+        said_to_grandma = input()
+   
+        if said_to_grandma == "":
+            print("WHAT!")
+        elif said_to_grandma == "GOODBYE!":
+            if goodbyes == 0:
+                goodbyes += 1
+                print("LEAVING SO SOON?")
+            elif goodbyes == 1:
+                goodbyes += 1
+                print("LATER, SKATER!")
+        elif said_to_grandma.isupper():
+            print("NO, NOT SINCE 1946!")
+        else:
+            print("SPEAK UP, KID!")
 
 
 
