43c43af83a57e6aeb98b33752e031cf041b5e154
ver2point0 <ver2point0@users.noreply.github.com>
2021-05-27 00:39:58 +0000

    John M deaf_grandma.py

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,21 @@
 def deaf_grandma():
-
-
-
+	counter = 0
+	print("HEY KID!")
+	
+	while (counter != 2):
+		user_input = input()
+		if (user_input == ""):
+			print("WHAT?!")
+		elif (user_input == "GOODBYE!"):
+			if (counter == 0):
+				print("LEAVING SO SOON?")
+				counter += 1
+			elif (counter == 1):
+				print("LATER, SKATER!")
+				counter += 1
+		elif (user_input == user_input.upper()):
+			print("NO, NOT SINCE 1946!")
+		else:
+			print("SPEAK UP, KID!")
+			
 deaf_grandma()
