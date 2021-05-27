d0a2337c26648cd0afe9ec49933106b0bb93fab7
jung341416 <jung341416@gmail.com>
2021-05-27 01:49:40 -0400

    last commit

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,10 +1,25 @@
-def deaf_grandma(text):
-    text = input("enter your answer : ")
+goodbye_count = 0
 
 
+def deaf_grandma():
+    global goodbye_count
+    prompt = input("Say Something?")
+
+    if prompt == '':
+        print('WHAT?!')
+        deaf_grandma()
+    elif prompt == prompt.upper() and prompt != "GOODBYE!":
+        print('NO, NOT SINCE 1946!')
+        deaf_grandma()
+    elif prompt == "GOODBYE!" and goodbye_count == 0:
+        print('LEAVING SO SOON?')
+        goodbye_count = goodbye_count + 1
+        deaf_grandma()
+    elif prompt == "GOODBYE!" and goodbye_count == 1:
+        print('LATER, SKATER!')
+    else:
+        print('SPEAK UP, KID!')
+        deaf_grandma()
 
 
-
-
-
-    deaf_grandma()
+deaf_grandma()
