cc4557ec20cf43479132710831767bca42c0ed05
Jonas Paulikas <70077913+paulikas78@users.noreply.github.com>
2021-06-07 12:30:31 -0500

    deaf grandma solved (finally) in python

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,25 @@
-function deafGrandma() {
+function deafGrandma(greeting) {
+  var text;
 
+let response = prompt("say something to your grandma");
+
+  switch(response) {
+  case "":
+    text = "WHAT?!";
+    break;
+  case response.toLowerCase();
+    text = "SPEAK UP, KID!";
+    break;
+  case response.toUpperCase():
+    text = "NO, NOT SINCE 1946!";
+    break;
+    case "Goodbye":
+    text = "LEAVING SO SOON?";
+    break;
+  default:
+    text = "LATER, SKATER!";
+    break;
+}
 }
 
-deafGrandma();
+deafGrandma()
